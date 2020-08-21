71443ce96ed6d16b5373bea9e1bdaf030af40c28
Max Bregman <bregmanmax91@gmail.com>
2020-08-20 20:13:35 -0600

    test book

--- src/book.js
+++ src/book.js
@@ -1,12 +1,54 @@
-function createTitle() {
-  
+function createTitle(bookName) {
+  var bookIdea = `The ${bookName}`
+
+  return bookIdea
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var ghoulCharacter = {
+    name: "Vassya",
+    age: 16,
+    pronouns: "she/her"
+}
+  return ghoulCharacter;
+}
+
+function saveReview(title, review) {
+  if (!review.includes(title)) {
+  return review.push(title);
+  }
+};
+
+function calculatePageCount(bookName) {
+  bookPageCount = bookName.length * 20;
+
+  return bookPageCount;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  }
+
+  return book
+}
+
+function editBook(bookTitle) {
+     bookTitle.pageCount = bookTitle.pageCount * .75
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
+//[bookIdea, sushiTitle, dragonTitle, ghostTitle]
+//
+//var dragonTitle = "Dragon in the Summer";
+//var ghostTitle = "The Teenage Soul";
