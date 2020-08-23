606cd4ce434645be80b4f4438b1976d7a59e7b79
Max Bregman <bregmanmax91@gmail.com>
2020-08-23 15:21:14 -0600

    add features for shelf and library and unskipped test functions

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,64 @@
+function createLibrary(libraryName) {
+  return {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+}
+
+function addBook(libraryName,newBook) {
+  libraryName.shelves[newBook.genre].push(newBook);
+}
+
+// libraryName.shelves.fantasy.push(newBook)
+// } else if (newBook.genre === 'fiction') {
+// libraryName.shelves.fiction.push(newBook)
+// } else if (newBook.genre === 'nonFiction') {libraryName.shelves.nonFiction.push(newBook)
+// }
 
 
+function checkoutBook(libraryName, checkedBook) { 
+
+  for (var i = 0; i < libraryName.shelves.fiction.length; i++) { 
+    if (checkedBook == libraryName.shelves.fiction[i].title) {  libraryName.shelves.fiction.splice(i, 1); 
+      return `You have now checked out ${checkedBook} from the ${libraryName.name}`; 
+      }
+    } 
+
+  for (var i = 0; i < libraryName.shelves.fantasy.length; i++) { 
+     if (bookName == libraryName.shelves.fantasy[i].title) {  libraryName.shelves.fantasy.splice(i, 1); 
+       return `You have now checked out ${checkedBook} from the ${libraryName.name}` 
+      }
+   } 
+
+for (var i = 0; i < libraryName.shelves.nonFiction.length; i++) {
+  if (checkedBook == libraryName.shelves.nonFiction[i].title) {       libraryName.shelves.nonFiction.splice(i, 1); 
+  return `You have now checked out ${checkedBook} from the ${libraryName.name}` 
+      } 
+    }
+    return `Sorry, there are currently no copies of ${checkedBook} available at the ${libraryName.name}`
+  }  
+
+
+
+
+
+
+
+// for (var i = 0; i < libraryName.shelves.length; i++)
+//   if (checkedBook === libraryName.shelves.fantasy[i].title) {
+//   libraryName.shelves.fantasy.splice(i, 1)
+// } else if (checkedBook === libraryName.shelves.nonFiction[i].title) {
+//   libraryName.shelves.nonFiction.splice(i, 1)
+// } else if(checkedBook === libraryName.shelves.fiction[i].title) {
+//   libraryName.shelves.fiction.splice(i, 1)
+// }
+// return `You have now checked out ${checkedBook} from the ${libraryName}`
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
-  // checkoutBook: checkoutBook
+  createLibrary: createLibrary,
+  addBook: addBook,
+  checkoutBook: checkoutBook
 };
--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,44 @@
+function shelfBook(book, sciFiShelf) {
+  if (book.genre === 'sciFi') {
+    sciFiShelf.unshift(book)
+  };
+  if (sciFiShelf.length > 3) {
+    sciFiShelf.shift()
+  };
+};
+
+function unshelfBook(bookTitle, sciFiShelf) {
+for (var i = 0; i < sciFiShelf.length; i++) {
+  if(sciFiShelf[i].title === bookTitle) {
+    sciFiShelf.splice(i,1);
+    }
+  }
+};
+
+function listTitles(shelf) {
+var titles = [];
+  for (var i = 0; i < shelf.length; i ++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.join(", ");
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true;
+    }
+  }
+  return false;
+};
 
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
+
+
+//sciFiShelf.splice(sciFiShelf.indexOf(book))
